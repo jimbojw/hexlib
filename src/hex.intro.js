323b253e8863109ecaa448ac810910845cdd7ae8
jimbojw <wilson.jim.r@gmail.com>
2010-05-28 23:57:08 -0400

    moving preamble bit to its own file

--- src/hex.intro.js
+++ src/hex.intro.js
@@ -1,12 +1,1 @@
-/*!
- * hexlib JavaScript Library v@VERSION
- * http://hexlib.com
- *
- * Copyright 2010, Jim R. Wilson
- * Licensed under the MIT license.
- * http://hexlib.com/license
- *
- * Date: 
- */
 (function( window, document, undefined ) {
-
---
+++ src/hex.license.js
@@ -0,0 +1,10 @@
+/*!
+ * hexlib JavaScript Library v@VERSION
+ * http://hexlib.com
+ *
+ * Copyright 2010, Jim R. Wilson
+ * Licensed under the MIT license.
+ * http://hexlib.com/license
+ *
+ * Date: 
+ */
