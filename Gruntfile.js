bbae7360165674071e074b9b2e0442247d892f1c
Jim R. Wilson <wilson.jim.r@gmail.com>
2013-08-09 15:42:22 -0400

    switching from Makefile based build to grunt

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    
+    clean: ['dist'],
+    
+    concat: {
+      dist: {
+        src: [
+          'src/hex.intro.js',
+          'src/hex.core.js',
+          'src/hex.element.js',
+          'src/hex.event.js',
+          'src/hex.grid.js',
+          'src/hex.grid.hexagonal.js',
+          'src/hex.grid.rectangular.js',
+          'src/hex.grid.skew.js',
+          'src/hex.grid.triangular.js',
+          'src/hex.region.js',
+          'src/hex.sprite.js',
+          'src/hex.outro.js'
+        ],
+        dest: 'dist/hex.js'
+      }
+    },
+    
+    uglify: {
+      dist: {
+        src: 'dist/hex.js',
+        dest: 'dist/hex.min.js'
+      }
+    }
+    
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['clean', 'concat', 'uglify']);
+
+};
